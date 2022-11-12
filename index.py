501818b78ef42ef06ac314e1807242a389c7a8f5
Toanf2103 <nobitakute002@gmail.com>
2022-11-12 15:52:50 +0700

    Toanf xong getAll

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify
-from team import getTeam
+from Module.team import getTeam
+from Module import Match
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def get_news(): 
     return getTeam.getAll()
-
+@app.route("/get", methods=["GET"])
+def getAll():
+    return jsonify( Match.getAllMatch())
 if __name__ == "__main__":
     app.run()
